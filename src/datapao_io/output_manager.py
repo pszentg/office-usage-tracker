4a937501c2887bd31b70cc1da1bab287bd901ea0
Pal Szentgyorgyi <pszentg@gmail.com>
2024-07-10 18:42:36 +0200

    added getting the longest work session

--- src/datapao_io/output_manager.py
+++ src/datapao_io/output_manager.py
@@ -12,9 +12,22 @@ class OutputManager:
         if not os.path.exists(os.path.dirname(output_location)):
             os.makedirs(os.path.dirname(output_location))
 
-        with open(output_location, mode="w", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=headers)
-            writer.writeheader()
-            for row in data:
-                writer.writerow(row)
-        logging.info(f"Data written to {output_location}")
+        try:
+            with open(output_location, mode="w", newline="") as file:
+                writer = csv.DictWriter(file, fieldnames=headers)
+                writer.writeheader()
+
+                # Check if data is a list of dictionaries or a single dictionary
+                if isinstance(data, list):
+                    for row in data:
+                        writer.writerow(row)
+                elif isinstance(data, dict):
+                    writer.writerow(data)
+                else:
+                    raise ValueError(
+                        "Data is neither a list of dictionaries nor a single dictionary"
+                    )
+
+            logging.info(f"Data written to {output_location}")
+        except Exception as e:
+            logging.error(f"Failed to save data to CSV: {e}")
