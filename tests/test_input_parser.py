9cf30e6570083a20760c4155655d19ddee3bc5f0
Pal Szentgyorgyi <pszentg@gmail.com>
2024-07-10 20:04:19 +0200

    added further tests

--- tests/test_input_parser.py
+++ tests/test_input_parser.py
@@ -3,15 +3,21 @@ from src.datapao_io.input_parser import InputParser
 
 
 class TestInputParser(unittest.TestCase):
-    def setUp(self):
-        self.input_file = "./resources/datapao_homework_2024.csv"
 
     def test_parse_input(self):
-        parsed_data = InputParser.parse_input(self.input_file)
+        input_file = "./resources/datapao_homework_2024.csv"
+        parsed_data = InputParser.parse_input(input_file)
         self.assertTrue(len(parsed_data) > 0)
         self.assertIn("user_id", parsed_data[0])
         self.assertIn("event_type", parsed_data[0])
         self.assertIn("event_time", parsed_data[0])
+
+    def test_input_does_not_exist(self):
+        input_file = "./non-existent-path.csv"
+        with self.assertRaises(OSError) as context:
+            InputParser.parse_input(input_file)
+
+        self.assertTrue("File does not exist!" in str(context.exception))
 
 
 if __name__ == "__main__":
--- tests/test_user.py
+++ tests/test_user.py
@@ -15,12 +15,41 @@ class TestUser(unittest.TestCase):
         )
 
     def test_incorrect_user_id(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as context:
             User("1")
+
+        self.assertTrue("Invalid user ID" in str(context.exception))
 
     def test_add_to_attendance(self):
         self.user.add_to_attendance("GATE_IN", "2024-01-01T08:00:00.000Z")
         self.assertEqual(len(self.user.attendance_data), 1)
+
+    def test_incorrect_gate_in_sequence(self):
+        with self.assertRaises(ValueError) as context:
+            self.user.add_to_attendance("GATE_IN", "2024-01-01T08:00:00.000Z")
+            self.user.add_to_attendance("GATE_IN", "2024-01-01T09:00:00.000Z")
+
+        self.assertTrue(
+            "GATE_IN must be preceded by GATE_OUT" in str(context.exception)
+        )
+
+    def test_incorrect_gate_out_sequence(self):
+        with self.assertRaises(ValueError) as context:
+            self.user.add_to_attendance("GATE_IN", "2024-01-01T08:00:00.000Z")
+            self.user.add_to_attendance("GATE_OUT", "2024-01-01T09:00:00.000Z")
+            self.user.add_to_attendance("GATE_OUT", "2024-01-01T10:00:00.000Z")
+
+        self.assertTrue(
+            "GATE_OUT must be preceded by GATE_IN" in str(context.exception)
+        )
+
+    def test_incorrect_gate_out_as_first_entry(self):
+        with self.assertRaises(ValueError) as context:
+            self.user.add_to_attendance("GATE_OUT", "2024-01-01T08:00:00.000Z")
+
+        self.assertTrue(
+            "The first attendance record must be 'GATE_IN'" in str(context.exception)
+        )
 
     def test_get_attendance_statistics(self):
         self.user.add_to_attendance("GATE_IN", self.start_time)
