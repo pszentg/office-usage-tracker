11243ab93b4789ef69fd2dea0a4e4d1a66596ce0
Pal Szentgyorgyi <pszentg@gmail.com>
2024-07-30 14:09:22 +0200

    added txt parser

--- src/datapao_io/input_parser.py
+++ src/datapao_io/input_parser.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import string
+from typing import List
 
 
 class InvalidCSVError(Exception):
@@ -15,19 +17,18 @@ class InputParser:
     REQUIRED_HEADERS = {"user_id", "event_type", "event_time"}
 
     @staticmethod
-    def parse_input(input_path):
+    def parse_input(input_path: string) -> List:
         if not os.path.exists(input_path):
             raise OSError("File does not exist!")
 
         if ".csv" in input_path:
             return InputParser.parse_csv_input(input_path)
 
-        # we could implement other types of input here
+        if ".txt" in input_path:
+            return InputParser.parse_txt_input(input_path)
 
     @staticmethod
-    def parse_csv_input(input_path):
-        if not os.path.exists(input_path):
-            raise FileNotFoundError(f"{input_path} does not exist")
+    def parse_csv_input(input_path: string) -> List:
 
         with open(input_path, mode="r", newline="") as file:
             reader = csv.DictReader(file)
@@ -39,5 +40,12 @@ class InputParser:
                 )
 
             data = [row for row in reader]
+
+        return data
+
+    @staticmethod
+    def parse_txt_input(input_path: string) -> List:
+        with open(input_path, mode="r", newline="") as file:
+            data = [row for row in file]
 
         return data
